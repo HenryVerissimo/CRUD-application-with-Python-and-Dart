4d1dcaf09457e71c00d506752ff31afca77c3e22
Henrique Verissimo <Henriquedevsp@gmail.com>
2025-10-13 19:58:44 -0300

    feat(route): add user route in app
    - include all /users routes in the application factory.

--- crud_backend/main.py
+++ crud_backend/main.py
@@ -2,7 +2,17 @@ from fastapi import FastAPI
 
 
 def create_app() -> FastAPI:
+    """FastAPI application Factory.
+    
+    Returns:
+        FastAPI: An instance of the FastAPI application.
+    """
+
     app = FastAPI()
+
+    from src.routes.user_routes import user_router
+
+    app.include_router(user_router)
 
     return app
 
