59a599c623de49232a9ff3589dfaba30e66c136a
Henrique Verissimo <Henriquedevsp@gmail.com>
2025-10-13 17:58:22 -0300

    feat(route): Add route to create new users.
    - add POST /users endpoint in API.
    - validate email, password and username entries with regex.

---
+++ crud_backend/src/routes/user_routes.py
@@ -0,0 +1,30 @@
+from fastapi import APIRouter, HTTPException
+
+from src.schemas.user_schemas import CreateUserSchema
+from src.controllers.user_controller import UserController
+
+from src.utils import regex_validations as regex
+
+
+user_router = APIRouter(prefix="/users", tags=["users"])
+
+
+@user_router.post("/")
+async def create_user(user_schema: CreateUserSchema):
+    """Creates a new user in the databse."""
+
+    if not regex.validate_email(user_schema.email):
+        raise HTTPException(status_code=400, detail="The email pattern is not valid")
+
+    if not regex.validate_password(user_schema.password):
+        raise HTTPException(status_code=400, detail="The password pattern is not valid")
+
+    if not regex.validate_username(user_schema.username):
+        raise HTTPException(
+            status_code=400, detail="The username pasttern is not valid"
+        )
+
+    controller = UserController()
+    response = controller.create_user(user_schema)
+
+    return response
