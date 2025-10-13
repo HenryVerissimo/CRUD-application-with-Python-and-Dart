f73400380670ab05d87a82c47c602cf25452076d
Henrique Verissimo <Henriquedevsp@gmail.com>
2025-10-13 18:22:55 -0300

    feat(repository): Create a user repository.
    - add UserRepository to administer database operations related to users.

---
+++ crud_backend/src/repositories/user_repository.py
@@ -0,0 +1,42 @@
+from src.models.user_model import User
+from src.schemas.user_schemas import CreateUserSchema
+from src.database.connection_db_interface import ConnectionDBInterface
+
+
+class UserRepository:
+    """Responsible for performing queries and database operations related to Users."""
+
+    def __init__(self, connection_db: ConnectionDBInterface) -> None:
+        """construct method this class
+
+        Args:
+            connection_db(ConnectionDBInterface): any class database connection that implements this interface.
+
+        Attributes:
+            connection_db(ConnectionDBInterface): Context manager used to connect to the database in the methods.
+        """
+
+        self.connection_db = connection_db
+
+    def create_user(self, user_schema: CreateUserSchema) -> User:
+        """Creates a new user in the database.
+
+        Args:
+            user_schema(CreateUserSchema): Object with user data.
+
+        Returns:
+            User: New user created.
+        """
+
+        with self.connection_db as connection:
+            new_user = User(
+                username=user_schema.username,
+                email=user_schema.email,
+                password=user_schema.password,
+            )
+
+            connection.session.add(new_user)  # type: ignore
+            connection.session.commit()  # type: ignore
+            connection.session.refresh(new_user)  # type: ignore
+
+            return new_user
