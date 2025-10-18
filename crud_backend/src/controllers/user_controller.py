813c68e233741c479fa152e73fd2157b37a56673
Henrique Verissimo <Henriquedevsp@gmail.com>
2025-10-18 17:35:54 -0300

    feat(controller): Create get users method.
    - add get_all_users() method in UserController.

--- crud_backend/src/controllers/user_controller.py
+++ crud_backend/src/controllers/user_controller.py
@@ -1,8 +1,12 @@
+from typing import List
+from fastapi import status
 from fastapi.responses import JSONResponse
 
-from src.schemas.user_schemas import CreateUserSchema
+from src.models.user_model import User
 from src.repositories.user_repository import UserRepository
 from src.database.mysql.connection_mysql_db import CONNECTION_MYSQL
+from src.exceptions.database import ErrorSelectingRecordsFromDatabase
+from src.schemas.user_schemas import CreateUserSchema
 
 
 class UserController:
@@ -31,3 +35,21 @@ class UserController:
             "data": {"id": data.id, "username": data.username},
         }
         return JSONResponse(content=payload, status_code=200)
+
+    def get_all_users(self) -> List[User]:
+        """Get all users in database.
+
+        Retuns:
+            List[User]: a list of objects with all users in the database.
+        """
+        try:
+            data = self.user_repo.select_users()
+
+        except Exception as error:
+            raise ErrorSelectingRecordsFromDatabase(
+                detail="Error trying to select users from database",
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                original_exception=error,
+            )
+
+        return data
