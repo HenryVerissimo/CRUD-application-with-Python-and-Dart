277c52af7d7230fb572f867cba34053efa740551
Henrique Verissimo <Henriquedevsp@gmail.com>
2025-10-18 18:57:29 -0300

    feat(application factory): Add register exception handler.
    - add register exception handler in create_app() function and register handlers.

--- crud_backend/main.py
+++ crud_backend/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 
+from src.handlers.register_hadlers import register_exception_handeler
+
 
 def create_app() -> FastAPI:
     """FastAPI application Factory.
@@ -12,7 +14,10 @@ def create_app() -> FastAPI:
 
     from src.routes.user_routes import user_router
 
+    register_exception_handeler(app)
     app.include_router(user_router)
+
+    app.add_exception_handler
 
     return app
 
