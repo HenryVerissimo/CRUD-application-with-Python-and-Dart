fea4e2ee69f50b351d1385f322542b2f9e321cbd
Henrique Verissimo <Henriquedevsp@gmail.com>
2025-10-19 17:35:55 -0300

    feat(controller): Create update user data method.
    - add update_user_data()  method in UserController.

--- crud_backend/src/controllers/user_controller.py
+++ crud_backend/src/controllers/user_controller.py
@@ -4,11 +4,13 @@ from fastapi import status
 from src.models.user_model import User
 from src.repositories.user_repository import UserRepository
 from src.database.mysql.connection_mysql_db import CONNECTION_MYSQL
-from src.schemas.user_schemas import CreateUserSchema
+from src.schemas.user_schemas import CreateUserSchema, UserUpdate
 from src.exceptions.database import (
     ErrorSelectingRecordsFromDatabase,
     ErrorCreatingRecordInDatabase,
     ErrorEmailAlreadyExists,
+    ErrorRecordNotFound,
+    ErrorPerformingDatabaseOperation,
 )
 
 
@@ -69,3 +71,41 @@ class UserController:
             )
 
         return data
+
+    def update_user_data(self, user_id: int, user_update_schema: UserUpdate) -> User:
+        """update user data in the database.
+
+        Args:
+            user_id(int): unique id of the user who will have their data updated.
+            user_update_schema(UserUpdate): Object with user data that will be updated.
+
+        Returns:
+            User: User object with its data.
+        """
+
+        try:
+            if user_update_schema.email is not None:
+                user = self.user_repo.select_user_by_email(user_update_schema.email)
+
+                if user is not None:
+                    raise ErrorEmailAlreadyExists(
+                        detail="This email has already been used",
+                        status_code=status.HTTP_409_CONFLICT,
+                    )
+
+            data = self.user_repo.update_user(user_id, user_update_schema)
+
+            if not data:
+                raise ErrorRecordNotFound(
+                    detail="Record not found in database",
+                    status_code=status.HTTP_404_NOT_FOUND,
+                )
+
+            return data
+
+        except Exception as error:
+            raise ErrorPerformingDatabaseOperation(
+                detail="Error trying to update user from database",
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                original_exception=error,
+            )
