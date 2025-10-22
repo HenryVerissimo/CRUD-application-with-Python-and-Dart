2ad51ec0a59f508a4b7c73fca81251da77a96518
Henrique Verissimo <Henriquedevsp@gmail.com>
2025-10-22 14:04:31 -0300

    feat(test): Create new integration test.
    - add test user update in the database.

--- crud_backend/tests/integration/test_user_repository.py
+++ crud_backend/tests/integration/test_user_repository.py
@@ -1,5 +1,6 @@
 import pytest
 
+from src.schemas.user_schemas import UserUpdate
 from src.repositories.user_repository import UserRepository
 
 
@@ -14,6 +15,25 @@ def test_create_and_validate_user_fields(
         expected = {"username": user.username, "email": user.email}
 
         assert expected == {"username": "TestUser", "email": "TestUser@gmail.com"}
+
+    else:
+        pytest.fail(
+            "the test failed because the test user was not found in the database"
+        )
+
+
+def test_user_update_in_the_database(
+    repository: UserRepository, create_test_user
+) -> None:
+    user_data = UserUpdate(username="UpdatedUser", email="UpdatedUser@gmail.com")
+
+    repository.update_user(1, user_data)
+
+    user = repository.select_user_by_id(1)
+
+    if user is not None:
+        assert user.username == "UpdatedUser"
+        assert user.email == "UpdatedUser@gmail.com"
 
     else:
         pytest.fail(
