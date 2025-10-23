fdac4da1b1b852e4fc67bc61b6e750fc0df008fb
Henrique Verissimo <Henriquedevsp@gmail.com>
2025-10-23 20:11:11 -0300

    feat(tests): create new unit test.
    - create test_what_the_create_users_route_returns() test.
    - ensures user creation route exists and returns valid status_code and payload.

---
+++ crud_backend/tests/unit/test_user_routes.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI
+from fastapi.testclient import TestClient
+
+
+from src.dependencies.controllers import get_user_controller
+from tests.mocks.user_controller_mock import CreateUserSuccessfully
+
+
+def test_what_the_create_users_route_returns(
+    get_client: TestClient, get_app: FastAPI
+) -> None:
+    get_app.dependency_overrides[get_user_controller] = CreateUserSuccessfully
+
+    payload = {
+        "username": "TestUser",
+        "email": "TestUser@gmail.com",
+        "password": "TestUserPassword123@",
+        "confirm_password": "TestUserPassword123@",
+    }
+
+    expected_response = {"id": 1, "username": "TestUser", "email": "TestUser@gmail.com"}
+
+    response = get_client.post(url="/users/", json=payload)
+
+    assert response.status_code == 201
+    assert response.json() == expected_response
