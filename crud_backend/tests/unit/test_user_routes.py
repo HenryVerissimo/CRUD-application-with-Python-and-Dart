0d5df9b3fd14f08ca5bda4c056bb09701e7d0b9e
Henrique Verissimo <Henriquedevsp@gmail.com>
2025-10-24 14:59:20 -0300

    feat(tests): Create new unit tests.
    - add test_what_the_get_users_route_returns().
    - add test_what_the_update_users_route_returns().
    - add test_what_the_delete_users_route_returns().
    - ensures user select, update and delete xroutes exists and returns valid status_code and payload.

--- crud_backend/tests/unit/test_user_routes.py
+++ crud_backend/tests/unit/test_user_routes.py
@@ -24,3 +24,46 @@ def test_what_the_create_users_route_returns(
 
     assert response.status_code == 201
     assert response.json() == expected_response
+
+
+def test_what_the_get_users_route_returns(
+    get_client: TestClient, get_app: FastAPI
+) -> None:
+    get_app.dependency_overrides[get_user_controller] = MockingSuccessfulResponses
+    expected_response = [
+        {"id": 1, "username": "UserTest1@", "email": "usertest1@gmail.com"},
+        {"id": 2, "username": "UserTest2@", "email": "usertest2@gmail.com"},
+        {"id": 3, "username": "UserTest3@", "email": "usertest3@gmail.com"},
+    ]
+
+    response = get_client.get("/users/")
+
+    assert response.status_code == 200
+    assert response.json() == expected_response
+
+
+def test_what_the_update_users_route_returns(
+    get_client: TestClient, get_app: FastAPI
+) -> None:
+    get_app.dependency_overrides[get_user_controller] = MockingSuccessfulResponses
+    payload = {"username": "UpdatedTestName", "email": "updatedtest@gmail.com"}
+    expected_response = {
+        "id": 1,
+        "username": "UpdatedTestName",
+        "email": "updatedtest@gmail.com",
+    }
+
+    response = get_client.patch("/users/update/1", json=payload)
+
+    assert response.status_code == 200
+    assert response.json() == expected_response
+
+
+def test_what_the_delete_users_route_returns(
+    get_client: TestClient, get_app: FastAPI
+) -> None:
+    get_app.dependency_overrides[get_user_controller] = MockingSuccessfulResponses
+
+    response = get_client.delete("/users/delete/1")
+
+    assert response.status_code == 204
