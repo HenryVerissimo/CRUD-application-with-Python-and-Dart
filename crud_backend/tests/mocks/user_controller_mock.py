86ced22b839329a94b5cd4bdf8693aed4e447598
Henrique Verissimo <Henriquedevsp@gmail.com>
2025-10-24 14:55:22 -0300

    chore(mock): Create new UserController mock methods.
    - add get_all_users method in MockingSuccessfulResponses.
    - add update_user_data method in MockingSuccessfulResponses.
    - add delete_user_record method in MockingSuccessfulResponses.

--- crud_backend/tests/mocks/user_controller_mock.py
+++ crud_backend/tests/mocks/user_controller_mock.py
@@ -1,6 +1,7 @@
 from abc import ABC
+from typing import List
 
-from src.schemas.user_schemas import CreateUserSchema, UserResponse
+from src.schemas.user_schemas import CreateUserSchema, UserResponse, UserUpdate
 
 
 class UserControllerMock(ABC):
@@ -18,3 +19,29 @@ class MockingSuccessfulResponses(UserControllerMock):
         )
 
         return user
+
+    def get_all_users(self) -> List[UserResponse]:
+        users = [
+            UserResponse(id=1, username="UserTest1@", email="usertest1@gmail.com"),
+            UserResponse(id=2, username="UserTest2@", email="usertest2@gmail.com"),
+            UserResponse(id=3, username="UserTest3@", email="usertest3@gmail.com"),
+        ]
+
+        return users
+
+    def update_user_data(
+        self, user_id: int, user_update_schema: UserUpdate
+    ) -> UserResponse | None:
+        if (user_update_schema.username is not None) and (
+            user_update_schema.email is not None
+        ):
+            user = UserResponse(
+                id=int(user_id),
+                username=user_update_schema.username,
+                email=user_update_schema.email,
+            )
+
+            return user
+
+    def delete_user_record(self, user_id: int) -> None:
+        pass
