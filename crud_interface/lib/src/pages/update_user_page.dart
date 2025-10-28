51f90b4c3dc057a0c430e6af7d3c5d7f8962424e
Henrique Verissimo <Henriquedevsp@gmail.com>
2025-10-28 18:34:26 -0300

    feat(page): Create a new page in the application.
    - Add interface to update user data in the database.

---
+++ crud_interface/lib/src/pages/update_user_page.dart
@@ -0,0 +1,54 @@
+import 'package:crud_interface/src/widgets/custom_button.dart';
+import 'package:crud_interface/src/widgets/custom_text_field.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class UpdateUserPage extends StatefulWidget {
+  const UpdateUserPage({super.key});
+
+  @override
+  State<UpdateUserPage> createState() => UpdateUserPageState();
+}
+
+class UpdateUserPageState extends State<UpdateUserPage> {
+  var idController = TextEditingController();
+  var usernameController = TextEditingController();
+  var emailController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      spacing: 40,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Column(
+          children: [
+            SvgPicture.asset("assets/images/create_user_icon.svg", width: 60),
+            Text(
+              "UPDATE USER",
+              style: TextStyle(
+                color: Color.fromRGBO(255, 255, 255, 1),
+                fontSize: 32,
+                fontFamily: "MonomaniacOne",
+              ),
+            ),
+          ],
+        ),
+        Column(
+          spacing: 5,
+          children: [
+            CustomTextField(label: "ID", controller: idController),
+            CustomTextField(label: "USERNAME", controller: usernameController),
+            CustomTextField(label: "EMAIL", controller: emailController),
+          ],
+        ),
+        CustomButton(
+          icon: Icon(Icons.update, color: Colors.black87),
+          text: "UPDATE",
+          color: Color.fromRGBO(77, 219, 124, 1),
+          onPressed: () {},
+        ),
+      ],
+    );
+  }
+}
